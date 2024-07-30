db6c9bf2280203391a4374254baab657828de987
Bura <brstive09@gmail.com>
2024-07-30 18:50:00 -0500

    First commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/sphero/sp/src
+# Build directory: /home/sphero/sp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ros-sphero-rvr/serial_lib")
+subdirs("ros-sphero-rvr/sphero_rvr")
+subdirs("ros-sphero-rvr/ros-sphero-rvr-msgs/sphero_rvr_msgs")
+subdirs("ros-sphero-rvr/sphero_rvr_movement")
+subdirs("ros-sphero-rvr/sphero_rvr_description")
+subdirs("ros-sphero-rvr/sphero_rvr_hw")
---
+++ src/ros-sphero-rvr/ros-sphero-rvr-msgs/README.md
@@ -0,0 +1,3 @@
+# ros-sphero-rvr-msgs
+This is the message repository for the ROS Sphero RVR integration. 
+Check out the main repository [ros-sphero-rvr](https://git.uibk.ac.at/informatik/stair/ros-sphero-rvr) for more information.
